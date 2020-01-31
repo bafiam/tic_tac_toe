c14ade7c3935e1a760cd35d4601ac745fb45007a
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-01-31 16:35:27 -0300

    added break to loop for testing; Board is being draw in a bi-dimensional grid now; turns now change after every move; board class now is in it's own file; Player class now it's just a struct

--- bin/main.rb
+++ bin/main.rb
@@ -1,24 +1,28 @@
 #!/usr/bin/ruby
-require_relative '../lib/game_logic.rb'
+require_relative '../lib/board.rb'
 
-puts "Welcome to the tic tac toe game"
-puts "Player 1: provide your name"
+Players = Struct.new(:name, :mark)
+
+puts "Welcome to the tic tac toe game\nPlayer 1: provide your name"
 player1 = gets.chomp
 puts "Player 2: provide your name"
 player2 = gets.chomp
-player_1 = Players.new(player1,"X")
-player_2 = Players.new(player2,"O")
+player_1 = Players.new(player1,"| X | ")
+player_2 = Players.new(player2,"| O | ")
 players = [player_1, player_2]
 puts "Welcome, #{player_1.name} and #{player_2.name}"
 puts ''
 play = Game_board.new
+const = 1
+turn = 0
 loop do
   play.display_grid
-  puts "#{players[0].name},Enter move between 1 and 9 :"
+  puts ""
+  puts "#{players[turn].name},Enter move between 1 and 9 :"
   move = gets.chomp
+  break if move == "-1"
   x, y = play.convert_input(move)
-  play.modify_cell(x, y, players[0].mark)
-  
+  play.modify_cell(x, y, players[turn].mark)
+  turn  += const
+  const *= -1
 end
-
-

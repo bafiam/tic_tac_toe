b1a66cf7fb91a725f914cc7ab9fbad7d560a6589
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-01-31 18:18:54 -0300

    added Win / Tie / Position Taken / Invalid input messages. They will pop in every input now since there is no logic yet; Win / Tie / Play again will appear after breaking the loop with -1

--- bin/main.rb
+++ bin/main.rb
@@ -15,9 +15,15 @@ loop do
   board.display_grid
   puts "\n#{players[turn].name}, Enter a number between 1 and 9 to make your move"
   move = gets.chomp
+  puts "That position has already been taken"
+  puts "That input is not valid. Write a number between 1 and 9"
   break if move == "-1"
   x, y = board.convert_input(move)
   board.modify_cell(x, y, players[turn].mark)
   turn  += const
   const *= -1
 end
+puts "#{players[turn].name} has won!!!!!!!!!!!!!!!!"
+puts "It's a Tie!!!!!!!!!!!!"
+puts "Do you want to play again? write Y for yes, N for no"
+

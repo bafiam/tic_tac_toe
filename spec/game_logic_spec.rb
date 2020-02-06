2bdf45bdd757a5b0d3f487105213b72f807953f6
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-02-06 00:52:34 -0300

    added loop, now checking every row, working

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -5,28 +5,24 @@ RSpec.describe Logic do
 	let(:logic) { Logic.new }
 	let(:board) { Game_board.new }
 	let(:grid)  { board.grid_key }
-	describe '#row' do
-		context "row 1" do
-			
-			it "X win" do
-				board.grid_key = [["| X | ", "| X | ", "| X | "]]
-				expect(logic.row(0, "| X | ", board)).to eq 3
+	describe '#check_win' do
+		context "checks wins and lose" do
+		  3.times do |x| 
+				it "X wins rows" do
+					board.grid_key = [["| X | ", "| X | ", "| X | "], ["| X | ", "| X | ", "| X | "], ["| X | ", "| X | ", "| X | "]]
+					expect(logic.row(x, "| X | ", board)).to eq 3
+				end
+				it "O win rows" do
+					board.grid_key = [["| O | ", "| O | ", "| O | "], ["| O | ", "| O | ", "| O | "], ["| O | ", "| O | ", "| O | "]]
+					expect(logic.row(x, "| O | ", board)).to eq 3
+				end
+				it "X does not win rows" do
+			  	expect(logic.row(0, "| X | ", board)).to eq 0
+				end
+				it "O does not win rows" do
+			  	expect(logic.row(0, "| O | ", board)).to eq 0
+				end
 			end
-			it "O win" do
-				board.grid_key = [["| O | ", "| O | ", "| O | "]]
-				expect(logic.row(0, "| O | ", board)).to eq 3
-			end
-			before do
-			  board.grid_key = [["| X | ", "| O | ", "| O | "]]
-			end
-			it "X does not win row" do
-			  expect(logic.row(0, "| X | ", board)).to eq 0
-			end
-			it "O does not win row" do
-			  expect(logic.row(0, "| O | ", board)).to eq 0
-			end
-
 		end
 	end
 end
-

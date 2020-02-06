6b7fe322ff42f925b0f2dd2e426587bd46f8ee47
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-02-06 17:46:56 -0300

    updating game_logic_spec with a few missing test cases

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require 'rspec'
+
 require_relative '../lib/game_logic.rb'
 require_relative '../lib/board.rb'
 RSpec.describe Logic do
@@ -7,33 +7,37 @@ RSpec.describe Logic do
   let(:board) { Game_board.new }
   let(:value) { ['| X | ', '| X | ', '| O | '] }
   describe '#check_win' do
-    context 'checks wins and lose' do
+    context 'checks wins' do
       before do
-        value = ["| X | ", "| X | ", "| X | "]
+        value = ['| X | ', '| X | ', '| X | ']
         board.grid_key = [value,  value, value]
       end
       it 'X wins diag' do
-        expect(logic.anti_diag("| X | ", board)).to eq 3
+        expect(logic.anti_diag('| X | ', board)).to eq 3
       end
       3.times do |x|
         it 'X winsm rows and cols' do
-          expect(logic.row(x, "| X | ", board)).to eq 3
-          expect(logic.col(x, "| X | ", board)).to eq 3
+          expect(logic.row(x, '| X | ', board)).to eq 3
+          expect(logic.col(x, '| X | ', board)).to eq 3
         end
         it 'O win' do
-          value = ["| O | ", "| O | ", "| O | "]
+          value = ['| O | ', '| O | ', '| O | ']
           board.grid_key = [value, value, value]
-          expect(logic.row(x, "| O | ", board)).to eq 3
+          expect(logic.row(x, '| O | ', board)).to eq 3
         end
+      end
+    end
+    context 'checks loses' do
+      before do
+        value = ['| X | ', '| X | ', '| O | ']
+        board.grid_key = [value,  value, value]
+      end
+      3.times do |x| 
         it 'X does not win row' do
-          value = ["| X| ", "| X | ", "| O | "]
-          board.grid_key = [value, value, value]
-          expect(logic.row(x, "| X | ", board)).to eq 0
+          expect(logic.row(x, '| X | ', board)).to eq 0
         end
         it 'O does not win rows' do
-          value = ["| O | ", "| O | ", "| X | "]
-          board.grid_key = [value, value, value]
-          expect(logic.row(x, "| O | ", board)).to eq 0
+          expect(logic.row(x, '| O | ', board)).to eq 0
         end
       end
     end
@@ -48,7 +52,7 @@ RSpec.describe Logic do
           expect(logic.manage_input(((x + 1) * -1).to_s)).not_to eq 1
         end
       end
-      var = "1"
+      var = '1'
       4.times do |x|
         var += x.to_s
         it 'check if input greater than 1 is incorrect (from 2 - 6)' do
